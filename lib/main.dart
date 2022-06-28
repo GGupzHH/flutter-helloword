2906681edde4024947b8ecf7c599e1f426e9d621
赵雅国 <1248023357@qq.com>
2022-06-28 23:43:34 +0800

    feat: codelab 04

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// 引入
 import 'package:english_words/english_words.dart';
 
 // 函数定义 和 参数定义
@@ -153,9 +154,23 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _biggerFont = const TextStyle(fontSize: 18);
+  final _suggestions = <WordPair>[];
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
+        // 除法
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(1));
+        }
+
+        return Text(_suggestions[index].asPascalCase);
+      }
+    );
   }
 }
